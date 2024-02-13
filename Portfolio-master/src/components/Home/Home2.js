8e0533b81fbd3dd1104d039f7aa0ea3481071efe
shima_dev <104167280+shimarrudz@users.noreply.github.com>
2024-02-13 15:11:42 -0300

    feat: my mail contact

--- Portfolio-master/src/components/Home/Home2.js
+++ Portfolio-master/src/components/Home/Home2.js
@@ -89,7 +89,7 @@ function Home2() {
               </li>
               <li className="social-icons">
                 <a
-                  href="vic.shima.vss@gmail.com"
+                  href="mailto:vic.shima.vss@gmail.com"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour home-social-icons"
