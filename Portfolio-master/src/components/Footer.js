e496187071dbde8b442a2c03fd779dc60680443f
shima_dev <104167280+shimarrudz@users.noreply.github.com>
2024-02-13 15:16:45 -0300

    chore: fix footer description

--- Portfolio-master/src/components/Footer.js
+++ Portfolio-master/src/components/Footer.js
@@ -13,7 +13,7 @@ function Footer() {
     <Container fluid className="footer">
       <Row>
         <Col md="4" className="footer-copywright">
-          <h3>Designed and Developed by Soumyajit Behera</h3>
+          <h3>Designed and Developed by Victor Shimada</h3>
         </Col>
         <Col md="4" className="footer-copywright">
           <h3>Copyright © {year} VSS</h3>
