0c0183ba728b20966cd3f4e586ba3b563ff6df2b
shima_dev <104167280+shimarrudz@users.noreply.github.com>
2024-02-15 22:33:35 -0300

    chore: change about description text

--- Portfolio-master/src/components/Home/Home2.js
+++ Portfolio-master/src/components/Home/Home2.js
@@ -19,18 +19,17 @@ function Home2() {
               LET ME <span className="purple"> INTRODUCE </span> MYSELF
             </h1>
             <p className="home-about-body">
-              I fell in love with programming and I have at least learnt
-              something, I think… 🤷‍♂️
+            I began my journey in programming in<b className="purple"> 2021</b>, and since then, I haven't stopped. Through continuous study, learning, and a lot of hard work, I've been advancing in this field.
               <br />
               <br />I am fluent in classics like
               <i>
-                <b className="purple"> Typescript, Javascript, Java, Python and C#. </b>
+                <b className="purple"> Typescript, Javascript, Java, Python and C#. </b> with frameworks <b className="purple"> Express, NestJS, Next.js, Spring, DJango, React and React Native</b> 
               </i>
               <br />
               <br />
               My field of Interest's are building new &nbsp;
               <i>
-                <b className="purple">Web Technologies and Products </b> and
+                <b className="purple">Web Technologies, Products </b> and
                 also in areas related to{" "}
                 <b className="purple">
                   Fintech.
@@ -38,18 +37,6 @@ function Home2() {
               </i>
               <br />
               <br />
-              Whenever possible, I also apply my passion for developing products
-              with <b className="purple">Node.js</b> and
-              <i>
-                <b className="purple">
-                  {" "}
-                  Modern Javascript Library and Frameworks
-                </b>
-              </i>
-              &nbsp; like
-              <i>
-                <b className="purple"> NestJS, Express, Srping, Django, React.js and Next.js</b>
-              </i>
             </p>
           </Col>
           <Col md={4} className="myAvtar">
